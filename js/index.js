4a8d8c7cb38d76476ae3e478e66d452988cc04d8
Matt Huntington <matt.huntington@gmail.com>
2017-05-04 15:38:24 -0400

    basic

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class About extends React.Component{
+    render(){
+        return <h1>About</h1>
+    }
+}
+
+class Topics extends React.Component{
+    render(){
+        return <h1>Topics</h1>
+    }
+}
 
 ReactDOM.render(
-    <h2>Matt is amazing</h2>,
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/topics">Topics</Link>
+                </li>
+            </ul>
+            <Route path="/about" component={About}/>
+            <Route path="/topics" component={Topics}/>
+        </div>
+    </Router>,
     document.querySelector('main')
 );
